cb7fdc1463772d2305c279d692d568ff3177a1bc
Zack Hartmann <zackhartmann@gmail.com>
2018-03-22 16:38:58 -0400

    first commit

---
+++ readDb.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seth
+ * Date: 3/22/18
+ * Time: 4:01 PM
+ */
+require('database.php');
+$db = new database("sqlite:////home/seth/PhpstormProjects/yahooSeleniumFE/coinsnapshot.db");
+
