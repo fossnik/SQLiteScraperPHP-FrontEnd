20225d74852cdb5aa4f4c22f39374c6c6c7a0f6e
Zack Hartmann <zackhartmann@gmail.com>
2018-03-22 16:41:52 -0400

    Annotation of required php-sqlite driver, and included gitignore

--- readDb.php
+++ readDb.php
@@ -5,6 +5,9 @@
  * Date: 3/22/18
  * Time: 4:01 PM
  */
+
+/** REQUIRED: php-sqlite driver */
+
 require('database.php');
 $db = new database("sqlite:////home/seth/PhpstormProjects/yahooSeleniumFE/coinsnapshot.db");
 
