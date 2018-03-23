687174fa69cf0c3b216560d762ed8fabe5b07cb0
Zack Hartmann <zackhartmann@gmail.com>
2018-03-23 12:57:57 -0400

    Echo All Coins

--- index.php
+++ index.php
@@ -11,12 +11,16 @@ use App\SQLiteConnection;
 
 $pdo = (new SQLiteConnection())->connect();
 if ($pdo != null)
-	echo 'Connected to the SQLite database successfully!' . "\n\n";
+	echo 'SQLite database connection acquired' . "\r\n";
 else
-	echo 'Whoops, could not connect to the SQLite database!' . "\n\n";
+	echo 'SQLite database Failure!' . "\r\n";
 
-$query = $pdo->query('SELECT * FROM btcusd');
+//$query = $pdo->query('SELECT * FROM btcusd');
+//
+//while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+//	print_r( $row );
+//}
 
-while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-	print_r( $row );
-}
+$query = $pdo->query("SELECT name FROM sqlite_master WHERE type='table';");
+while($coin = $query->fetch(PDO::FETCH_ASSOC))
+	echo $coin['name'] . "\r\n";
