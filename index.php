8f8c548d69d8e0dc3647247084357d057bbb471c
Zack Hartmann <zackhartmann@gmail.com>
2018-03-23 14:02:48 -0400

    Implemented an HTML option list, drawing from the SQL table names

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: seth
- * Date: 3/22/18
- * Time: 7:44 PM
- */
-require 'vendor/autoload.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Coins and Stuff</title>
+	<?php
+		require __DIR__ . '/dbQuery.php';
 
-use App\SQLiteConnection;
+		// declare a new database query object
+		$queryDb = new dbQuery;
+		?>
+</head>
+<body>
 
-$pdo = (new SQLiteConnection())->connect();
-if ($pdo != null)
-	echo 'SQLite database connection acquired' . "\r\n";
-else
-	echo 'SQLite database Failure!' . "\r\n";
+	<h1>Select Your Coin</h1>
+		<select name="country">
+			<option value="">---------</option>
+			<?php
+				foreach ($queryDb->getCoins() as $dbCoin)
+					echo '<option value="'.$dbCoin.'">'.$dbCoin.'</option>';
+				?>
+		</select>
 
-//$query = $pdo->query('SELECT * FROM btcusd');
-//
-//while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-//	print_r( $row );
-//}
-
-$query = $pdo->query("SELECT name FROM sqlite_master WHERE type='table';");
-while($coin = $query->fetch(PDO::FETCH_ASSOC))
-	echo $coin['name'] . "\r\n";
+</body>
+</html>
